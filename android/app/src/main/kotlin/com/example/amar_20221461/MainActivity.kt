f10ee76e077f67749cb439ff00ab48cc237a65a8
Amar <s8tangaming@gmail.com>
2023-12-06 19:51:58 +0530

    created quizapp in flutter

---
+++ android/app/src/main/kotlin/com/example/amar_20221461/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amar_20221461
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const primaryColor = Color(0xFF42A5F5);
+const accentColor = Color(0xFF2196F3);
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App Amar_20221461',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const QuizPage(),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int _currentQuestion = -1; // Start with -1 to handle the initial start
+  bool _quizEnded = false;
+  int _score = 0;
+  bool _quizStarted = false; // Track if the quiz has started
+
+  final List<Map<String, dynamic>> _questions = [
+    {
+      "question": "What is the capital of Bharat?",
+      "options": ["New Delhi", "Kolkata", "Karachi", "Ranchi"],
+      "answer": "New Delhi",
+    },
+    {
+      "question": "Who is First President of India",
+      "options": [
+        "Dr. BR Ambedkar",
+        "Mahatma Gandhi",
+        "Dr. Rajendra Prasad",
+        "Sardar Vallabhai Patel"
+      ],
+      "answer": "Dr. Rajendra Prasad",
+    },
+    {
+      "question": "Largest Mammal on Earth is?",
+      "options": ["Cow", "Shark", "Human", "Blue Whale"],
+      "answer": "Blue Whale",
+    },
+    {
+      "question":
+          "Is the statement True or False.\nIs the Great Wall Of China visible from space?\n",
+      "options": ["True", "False", "Maybe"],
+      "answer": "False",
+    },
+    {
+      "question": "Did BJP win elections in Telangana?",
+      "options": ["Yes", "No"],
+      "answer": "No",
+    },
+  ];
+
+  void _checkAnswer(String option) {
+    if (_questions[_currentQuestion]["answer"] == option) {
+      setState(() {
+        _score++;
+      });
+    }
+    _moveToNextQuestion();
+  }
+
+  void _moveToNextQuestion() {
+    setState(() {
+      _currentQuestion++;
+      _quizEnded = _currentQuestion == _questions.length;
+    });
+  }
+
+  void _moveToPreviousQuestion() {
+    setState(() {
+      if (_currentQuestion > 0) {
+        _currentQuestion--;
+        if (_quizEnded) {
+          _score--; // Adjust the score
+          _quizEnded = false;
+        }
+      }
+    });
+  }
+
+  void _startQuiz() {
+    setState(() {
+      _currentQuestion = 0;
+      _quizStarted = true;
+    });
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _currentQuestion = -1;
+      _quizEnded = false;
+      _score = 0;
+      _quizStarted = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (!_quizStarted ||
+                      !_quizEnded) // Show only before quiz starts and if not ended
+                    AnimatedOpacity(
+                      opacity: _quizStarted ? 0.0 : 1.0,
+                      duration: const Duration(milliseconds: 500),
+                      child: Text(
+                        '\nWelcome to Quiz App!\n',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  const SizedBox(height: 20.0),
+                  if (_currentQuestion >= 0 && !_quizEnded)
+                    Text(
+                      "Question ${_currentQuestion + 1} of ${_questions.length}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  const SizedBox(height: 10.0),
+                  if (_quizEnded)
+                    Column(
+                      children: [
+                        Text(
+                          "You have completed the quiz!",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Your final score is $_score out of ${_questions.length}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    )
+                  else if (!_quizStarted)
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: accentColor,
+                        primary: primaryColor,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 20.0,
+                          horizontal: 40.0,
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: _startQuiz,
+                      child: const Text("Start Quiz"),
+                    )
+                  else
+                    Column(
+                      children: [
+                        Text(
+                          _questions[_currentQuestion]["question"] as String,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        ...(_questions[_currentQuestion]["options"]
+                                as List<String>)
+                            .map((option) => _optionButton(
+                                  option,
+                                  () {
+                                    if (!_quizEnded) {
+                                      _checkAnswer(option);
+                                    }
+                                  },
+                                ))
+                            .toList(),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ),
+          if (_quizEnded)
+            Positioned(
+              bottom: 16.0,
+              right: 16.0,
+              child: FloatingActionButton(
+                backgroundColor: accentColor,
+                onPressed: _resetQuiz,
+                child: const Icon(Icons.refresh),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _optionButton(String option, VoidCallback onPressed) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: primaryColor,
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        minimumSize: const Size(double.infinity, 50.0),
+        textStyle: const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(option),
+    );
+  }
+}
